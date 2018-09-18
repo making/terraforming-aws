bf351261cef709881fe7cb1debd738923acc2d42
Toshiaki Maki <tmaki@pivotal.io>
2018-09-19 00:23:38 +0900

    Update common.sh

--- om/common.sh
+++ om/common.sh
@@ -7,4 +7,4 @@ export OM_DECRYPTION_PWD=changeme
 
 
 export TF_DIR=$(dirname "$0")/..
-export OPSMAN_DOMAIN_OR_IP_ADDRESS=$(cat $TF_DIR/terraform.tfstate | jq -r '.modules[0].outputs.ops_manager_public_ip.value')
+export OPSMAN_DOMAIN_OR_IP_ADDRESS=$(cat $TF_DIR/terraform.tfstate | jq -r '.modules[0].outputs.ops_manager_dns.value')
