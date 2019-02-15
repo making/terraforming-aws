dbab963bd6d4e32ad0f601d83fa7e686468d2e90
Toshiaki Maki <tmaki@pivotal.io>
2019-02-16 00:21:25 +0900

    Update configure-pks.sh

--- om/configure-pks.sh
+++ om/configure-pks.sh
@@ -13,7 +13,7 @@ AVAILABILITY_ZONES=$(cat $TF_DIR/terraform.tfstate | jq -r '.modules[0].outputs.
 AVAILABILITY_ZONE_NAMES=$(cat $TF_DIR/terraform.tfstate | jq -r '.modules[0].outputs.azs.value' | tr -d '\n' | tr -d '"')
 
 WILDCARD_DOMAIN=`echo ${OPSMAN_DOMAIN_OR_IP_ADDRESS} | sed 's/pcf/*/g'`
-CERTIFICATES=`om generate-certificate -ds ${WILDCARD_DOMAIN}`
+CERTIFICATES=`om generate-certificate -d ${WILDCARD_DOMAIN}`
 CERT_PEM=`echo $CERTIFICATES | jq -r '.certificate' | sed 's/^/        /'`
 KEY_PEM=`echo $CERTIFICATES | jq -r '.key' | sed 's/^/        /'`
 INSTANCE_PROFILE_MASTER=$(cat $TF_DIR/terraform.tfstate | jq -r '.modules[0].outputs.pks_master_instance_profile_name.value')
